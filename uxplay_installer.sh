a200f8a039ee6973a4400fd06adc14f9525b8626
Philip Dell <62812264+pIlIp-d@users.noreply.github.com>
2022-10-02 14:10:29 +0200

    added auto install via -y option

--- uxplay_installer.sh
+++ uxplay_installer.sh
@@ -12,10 +12,10 @@ fi
 # guide from https://montessorimuddle.org/2020/12/05/uxplay-sharing-ipad-screen-on-linux-ubuntu/
 
 # Install packages
-apt-get install cmake
+apt-get install cmake -y
 
 apt-get install libssl-dev libavahi-compat-libdnssd-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav
-apt-get install gstreamer1.0-vaapi
+apt-get install gstreamer1.0-vaapi -y
 
 # clone repo
 [ ! -d ./UxPlay ] && git clone https://github.com/FDH2/UxPlay
